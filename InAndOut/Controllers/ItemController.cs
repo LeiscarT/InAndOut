e7a3cf1e2770fde5c2c61f07c579341fb9341967
LeiscarT <leiscartp29@gmail.com>
2021-09-01 19:14:06 -0400

    add migrations, itemController and Index

---
+++ InAndOut/Controllers/ItemController.cs
@@ -0,0 +1,25 @@
+﻿using InAndOut.Data;
+using InAndOut.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InAndOut.Controllers
+{
+    public class ItemController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public ItemController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Item> objList = _db.Items;
+            return View(objList);
+        }
+    }
+}
