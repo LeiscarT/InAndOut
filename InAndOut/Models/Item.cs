ea00f98da0b1539cfc76d13398150da0a5f95c1e
LeiscarT <leiscartp29@gmail.com>
2021-09-01 19:22:26 -0400

    add new fields in the model class

--- InAndOut/Models/Item.cs
+++ InAndOut/Models/Item.cs
@@ -11,5 +11,9 @@ namespace InAndOut.Models
         [Key]
         public int Id { get; set; }
         public string Borrower { get; set; }
+
+        public string Lender { get; set; }
+
+        public string ItemName { get; set; }
     }
 }
