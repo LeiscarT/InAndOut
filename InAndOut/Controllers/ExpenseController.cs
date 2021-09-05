653af8b80795fe70863dee33db2e530d21166927
LeiscarT <leiscartp29@gmail.com>
2021-09-05 19:02:52 -0400

    add viewbag of ExpenseType and Fk

--- InAndOut/Controllers/ExpenseController.cs
+++ InAndOut/Controllers/ExpenseController.cs
@@ -1,6 +1,7 @@
 ﻿using InAndOut.Data;
 using InAndOut.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -24,6 +25,9 @@ namespace InAndOut.Controllers
 
         public IActionResult Create()
         {
+            IEnumerable<SelectListItem> TypeDropDown = _db.ExpenseTypes.Select(i => new SelectListItem { Text = i.Name, Value = i.Id.ToString()});
+            ViewBag.TypeDropDown = TypeDropDown;
+
             return View();
         }
 
@@ -33,6 +37,7 @@ namespace InAndOut.Controllers
         {
             if (ModelState.IsValid)
             {
+                
                 _db.Expenses.Add(expense);
                 _db.SaveChanges();
                 return RedirectToAction("Index");
